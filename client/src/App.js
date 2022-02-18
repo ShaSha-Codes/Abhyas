fbc746bb4b8203001db7ee17746fd0a202841fea
Mihir <mihirtayshete@gmail.com>
2022-02-18 18:00:53 +0530

    temp edit

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
+import './style.css'
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -13,15 +14,18 @@ const Item = styled(Paper)(({ theme }) => ({
 
 export default function App(){
     return(
-            
-          <Grid container>
-            <Grid item xs={12} md={6}>
-                <Item><h1>Hello</h1></Item>
+            <div>
+          <Grid container className="index-body">
+            <Grid item xs={12} md={6} >
+                <Item disabled sx={{border:0,boxShadow:0,marginTop:'10%',bodyColor:'transparent'}} className="indexpart1"><h1>Abhyas</h1>
+                <h2>A place to learn and share</h2>
+                </Item>
             </Grid>
-            <Grid item xs={12} md={6}>
-                <Item><h1>There</h1></Item>
+            <Grid item xs={12} md={6} >
+                <Item sx={{border:0,boxShadow:0,}} className="indexpart2"><h1>There</h1></Item>
             </Grid>
         </Grid>
+        </div>
         
       
     )
