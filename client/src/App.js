9116c0dd316bf6ad62cfb502772f08357c456269
Mihir <mihirtayshete@gmail.com>
2022-02-18 16:10:48 +0530

    start edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function App(){
+    return(
+            
+          <Grid container>
+            <Grid item xs={12} md={6}>
+                <Item><h1>Hello</h1></Item>
+            </Grid>
+            <Grid item xs={12} md={6}>
+                <Item><h1>There</h1></Item>
+            </Grid>
+        </Grid>
+        
+      
+    )
 }
-
-export default App;
