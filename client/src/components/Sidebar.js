2009922482efd2a58adfe24aec713fc6bf52586e
Jazib Dawre <jazib980@gmail.com>
2022-02-19 22:18:01 +0530

    Add Sidebar

---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,59 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+
+import NavBar from "./NavBar";
+
+const drawerWidth = 240;
+
+export default function Sidebar() {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <NavBar />
+      {/* TODO: Make a copy of the below drawer with one 'temporary' for mobile devices */}
+      <Drawer
+        variant="permanent"
+        sx={{
+          display: { xs: "none", sm: "block" },
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: "border-box" },
+        }}
+      >
+        <Toolbar /> {/* So just for spacing? */}
+        <Box sx={{ overflow: "auto" }}>
+          <List>
+            {["Videos", "Assignments", "Quizzes", "Notes"].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {["Students"].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
+    </Box>
+  );
+}
