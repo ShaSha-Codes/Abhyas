8b5f19bb8585629c9a291441268f07a67b76f6a4
Mihir <mihirtayshete@gmail.com>
2022-02-23 22:05:34 +0530

    Changed style.css

--- client/src/layouts/App.js
+++ client/src/layouts/App.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import TeacherClass from "../pages/TeacherClass";
-import TeacherDashboard from "../pages/TeacherDashboard";
-import StudentClass from "../pages/StudentClass";
-import "../assets/css/style.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-export default function App() {
-  return (
-    <>
-      <TeacherClass />
-    </>
-  );
-}
-=======
 import React from 'react'
 import Main from '../pages/Main'
 export default function App(){
@@ -22,4 +6,3 @@ export default function App(){
             <Main/>
     )
 }
->>>>>>> Mihir
