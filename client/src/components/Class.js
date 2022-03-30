77f8118fbfbeeadcc5d676aec945c72d587d3574
Mihir <mihirtayshete@gmail.com>
2022-03-30 14:27:25 +0530

    border radius added

--- client/src/components/Class.js
+++ client/src/components/Class.js
@@ -10,9 +10,10 @@ function Class() {
     background: "rgb(229,228,249)",
     background: "linear-gradient(90deg, rgba(247,247,252,1) 0%, rgba(255,255,255,1) 35%, rgba(253,255,255,1) 100%)",
     boxShadow: "6px 6px 21px -14px rgba(77,77,77,1)",
+    borderRadius:"12px",
   };
   return (
-    <Grid item xs={7} sm={5} md={3} lg={2.5} sx={{display:"block",marginLeft:"auto",marginRight:"auto",borderRadius:"10px",borderRadius:"100px"}} >
+    <Grid item xs={7} sm={5} md={3} lg={2.5} sx={{display:"block",marginLeft:"auto",marginRight:"auto",borderRadius:"10px"}} >
       <Box>
         <Paper variant="outlined" sx={styler}>
           <Typography sx={{ position: "relative", top: "10px", left:"10px",marginBottom:"15px" }}>
