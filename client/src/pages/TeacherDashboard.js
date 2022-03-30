cc5bd20fe6c555d40d27420e1238004ed827eb58
Mihir <mihirtayshete@gmail.com>
2022-03-30 14:26:14 +0530

    basic card for /teacher

--- client/src/pages/TeacherDashboard.js
+++ client/src/pages/TeacherDashboard.js
@@ -2,18 +2,25 @@ import React from "react";
 import Navbar from "../components/NavBar";
 import Class from "../components/Class";
 import Grid from "@mui/material/Grid";
+import Box from '@mui/material/Box';
 
 function TeacherDashboard() {
   return (
     <>
       <Navbar />
       <br />
-      <Grid container spacing={5} justify="center">
+      <div className="gridContainer">
+      <Grid Grid container spacing={3} mt={1} justify="center">
+        
+        
         <Class />
         <Class />
         <Class />
         <Class />
+        
       </Grid>
+      </div>
+     
     </>
   );
 }
