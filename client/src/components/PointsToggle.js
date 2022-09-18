17ccd52ee45905d140c09ae5893f96adc3d90dfe
manmikalpha <manmikalpha@users.noreply.github.com>
2022-09-18 13:10:41 +0000

    Prettified Code!

--- client/src/components/PointsToggle.js
+++ client/src/components/PointsToggle.js
@@ -3,12 +3,11 @@ import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import BookIcon from '@mui/icons-material/Book';
+import BookIcon from "@mui/icons-material/Book";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router";
 
 function PointsToggle(props) {
-
   return (
     <Box sx={{ textAlign: "center" }}>
       <Typography variant="h5" sx={{ margin: "1em" }} component="h2">
@@ -18,15 +17,13 @@ function PointsToggle(props) {
         >
           <Button
             onClick={() => {
-              return (
-                props.setPointsVisibility(
-                  (prevPointsVisibility) => !prevPointsVisibility
-                )
+              return props.setPointsVisibility(
+                (prevPointsVisibility) => !prevPointsVisibility
               );
             }}
           >
             <BookIcon fontSize="large" />
-             Activity Diary
+            Activity Diary
           </Button>
         </ButtonGroup>
       </Typography>
